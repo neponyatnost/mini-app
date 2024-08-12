f97aa1ef85711a85aaef8d4413d87840e81d8d86
neponyatnost <iv.konuhov@gmail.com>
2024-08-12 14:07:04 +0400

    4th commit

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,47 +1,25 @@
-import { Cell, Image, List, Section } from '@telegram-apps/telegram-ui'
-import type { FC } from 'react'
-
 import { Link } from '@/components/Link/Link.tsx'
-
-import tonSvg from './ton.svg'
+import { Button, Cell, Image, List, Section } from '@telegram-apps/telegram-ui'
+import type { FC } from 'react'
+import tonImage from './ton.svg'
 
 export const IndexPage: FC = () => {
   return (
     <List>
-      <Section
-        header='Features'
-        footer='You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects'
-      >
+      <Section>
         <Link to='/ton-connect'>
           <Cell
-            before={
-              <Image src={tonSvg} style={{ backgroundColor: '#007AFF' }} />
-            }
-            subtitle='Connect your TON wallet'
+            before={<Image src={tonImage} size={48} />}
+            subtitle='Connect your TON wallet!'
           >
             TON Connect
           </Cell>
         </Link>
       </Section>
-      <Section
-        header='Application Launch Data EDITED!'
-        footer='These pages help developer to learn more about current launch information'
-      >
-        <Link to='/init-data'>
-          <Cell subtitle='User data, chat information, technical data'>
-            Init Data
-          </Cell>
-        </Link>
-        <Link to='/launch-params'>
-          <Cell subtitle='Platform identifier, Mini Apps version, etc.'>
-            Launch Parameters
-          </Cell>
-        </Link>
-        <Link to='/theme-params'>
-          <Cell subtitle='Telegram application palette information'>
-            Theme Parameters
-          </Cell>
-        </Link>
+      <Section>
+        <Cell>
+          <Button onClick={() => console.log('dfgd')}>Test</Button>
+        </Cell>
       </Section>
     </List>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-// import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +15,7 @@ export default defineConfig({
     tsconfigPaths(),
     // Allows using self-signed certificates to run the dev server using HTTPS.
     // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
-    // basicSsl(),
+    basicSsl(),
   ],
   publicDir: './public',
   server: {
